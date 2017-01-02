c94c252cddcdcd16bc3488c895ee63cfd06777a6
Tien Pham <tienpm1812@gmail.com>
2017-01-02 14:46:55 +0700

    add API controller for video bot

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vuonglinh
+ * Date: 1/2/17
+ * Time: 14:46
+ */
+
+namespace App\Controller;
+
+use Cake\Controller\Controller;
+use Cake\Event\Event;
+
+class ApiController extends Controller
+{
+
+}
